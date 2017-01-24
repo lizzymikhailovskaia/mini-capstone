349e2fe62c0c7d461199ced4cd304daf5c7be367
Lizzy <0572788@gmail.com>
2017-01-24 00:33:29 -0800

    added new font

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,10 +1,44 @@
 class ProductsController < ApplicationController
+    def index
+        @products = Product.all
+        render "index.html.erb"
+    end
 
-  def get_products
-    @products = Product.all
-    render "products.html.erb"
-  end
+    def new
+        render "new.html.erb"
+    end
+
+    def create
+        product = Product.create(name: params["name"], price: params["price"], description: params["description"], image: params["image"])
+        redirect_to "/products/#{product.id}"
+
+    end
+
+    def show
+        product_id = params[:id]
+        @product = Product.find_by(id:product_id)
+        render "show.html.erb"
+    end
+
+    def edit
+        render "edit.html.erb"
+    end
+
+    def update
+        products_id = params [:id]
+        product = Products.find_by(id:products_id)
+        product.update(name: params["name"], price: param["price"], description: params["description"], image: params["image"])
+        products.save
+        flash[:message] = "Update"
+        redirect_to "/products/#{product_id}"
+
+    end
+
+    def destroy
+       product_id = params[:id]
+       product = Product.find_by(id: product_id)
+       product.destroy
+       redirect_to "/products"
+    end
 
 end
-
-
