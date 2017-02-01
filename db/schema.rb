d5492a43d4fac01d87c0305b4a9a9680f937720c
Lizzy <0572788@gmail.com>
2017-01-31 19:45:06 -0800

    merge

--- db/schema.rb
+++ db/schema.rb
@@ -11,8 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 20170119033128) do
 
+=======
+ActiveRecord::Schema.define(version: 20170201034331) do
+>>>>>>> fd78849... added some auth
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +28,14 @@ ActiveRecord::Schema.define(version: 20170119033128) do
     t.string   "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
   create_table "users", force: :cascade do |t|
