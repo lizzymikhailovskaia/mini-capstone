735226c45c272f09e2557213bf7ef1e7479bfcc0
Lizzy <0572788@gmail.com>
2017-01-31 19:45:06 -0800

    merge

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+ActiveRecord::Schema.define(version: 20170119033128) do
+
+
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
